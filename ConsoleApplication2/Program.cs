2a35e32a116c0b532f998abb4540ab2c2b1e6443
Aniruddh Ravipati <ravipatianirudh@gmail.com>
2016-06-20 12:22:05 +0530

    Basic Trials

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ConsoleApplication2
 {
@@ -10,6 +11,40 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+            Stopwatch timer = new Stopwatch();
+            Console.WriteLine("Starting Async Trials");
+            timer.Start();
+            Task t = AsyncTrialMethod();
+            t.Wait();
+            timer.Stop();
+            Console.WriteLine("Ended Async Trials in " + timer.ElapsedMilliseconds + " milliseconds");
+            Console.ReadKey();
+        }
+
+        public static async Task AsyncTrialMethod()
+        {
+            Task t1 = FirstWaitTask();
+            Task t2 = SecondWaitTask();
+            Stopwatch timer = new Stopwatch();
+
+            timer.Start();
+            await t1;
+            await t2;
+            timer.Stop();
+
+            Console.WriteLine("Both Tasks Done in "+ timer.ElapsedMilliseconds + " milliseconds");
+        }
+
+        public static async Task FirstWaitTask()
+        {
+            Console.WriteLine("First");
+            await Task.Delay(5000);
+        }
+
+        public static async Task SecondWaitTask()
+        {
+            Console.WriteLine("Second");
+            await Task.Delay(5000);
         }
     }
 }
